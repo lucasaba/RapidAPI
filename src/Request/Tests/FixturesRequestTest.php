9c90055c7819abd7acc8f8fb60bcd48fdfe1724d
Luca Saba <lucasaba@gmail.com>
2021-09-23 19:26:32 +0200

    Updates test

--- src/Request/Tests/FixturesRequestTest.php
+++ src/Request/Tests/FixturesRequestTest.php
@@ -11,6 +11,7 @@ class FixturesRequestTest extends TestCase
     {
         $request = new FixturesRequest(2021);
         $request->withId(1)
+            ->withSeason(2021)
             ->withLeague(2)
             ->withTeam(3)
             ->withDate('2021-01-01')
